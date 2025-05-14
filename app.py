84862028f6f2ee0c828e75177af6233c3b30dcac
José <98433912+joserqn@users.noreply.github.com>
2025-05-14 18:18:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,27 @@ import gspread
 from google.oauth2.service_account import Credentials
 from st_aggrid import AgGrid, GridOptionsBuilder
 
+def autenticar_google_sheets():
+    scopes = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    info = {
+        "type": st.secrets["google_service_account"]["type"],
+        "project_id": st.secrets["google_service_account"]["project_id"],
+        "private_key_id": st.secrets["google_service_account"]["private_key_id"],
+        "private_key": st.secrets["google_service_account"]["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["google_service_account"]["client_email"],
+        "client_id": st.secrets["google_service_account"]["client_id"],
+        "auth_uri": st.secrets["google_service_account"]["auth_uri"],
+        "token_uri": st.secrets["google_service_account"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["google_service_account"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["google_service_account"]["client_x509_cert_url"],
+        "universe_domain": st.secrets["google_service_account"]["universe_domain"]
+    }
+    credentials = Credentials.from_service_account_info(info, scopes=scopes)
+    client = gspread.authorize(credentials)
+    return client
 # --- Autenticação Google Sheets ---
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets.readonly",
