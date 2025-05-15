634bfab0dcbb3bb854101cd1fa36583bf9b2ffb7
José <98433912+joserqn@users.noreply.github.com>
2025-05-14 22:02:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if user_code == ACCESS_CODE:
 
         # Verificações explícitas da private_key
         private_key = creds_dict.get("private_key")
-            if not private_key:
+                if not private_key:
             st.error("Erro: 'private_key' não encontrada nas credenciais.")
             st.stop()
                 if "-----BEGIN PRIVATE KEY-----" not in private_key or "-----END PRIVATE KEY-----" not in private_key:
